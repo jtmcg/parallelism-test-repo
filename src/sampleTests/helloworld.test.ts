d06e09c2d269f7492acbc6a0a48003e8bd98de4c
Tyler McGoffin <tyler.mcgoffin@circleci.com>
2022-02-17 14:51:52 -0800

    60 second timeout

--- src/sampleTests/helloworld.test.ts
+++ src/sampleTests/helloworld.test.ts
@@ -1,7 +1,8 @@
 import helloWorld from './helloworld';
 
 describe('/sampleTests/helloworld', () => {
-  it('should pass', () => {
+  it('should pass', async () => {
+    await new Promise(f => setTimeout(f, 60000));
     expect(helloWorld()).toBe("hello world");
   })
 });
